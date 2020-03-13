33c2d18b0c3ffa51f79601aeac65dd76581fc8bb
TASPRO IT Solutions <asikraja@tasproit.com>
2020-03-13 18:28:02 +0530

    first commit 13032020

---
+++ src/main/java/com/tasproit/gcp/DownloadObject.java
@@ -0,0 +1,46 @@
+package com.tasproit.gcp;
+import com.google.cloud.storage.Blob;
+import com.google.cloud.storage.BlobId;
+import com.google.cloud.storage.Storage;
+import com.google.cloud.storage.StorageOptions;
+
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+public class DownloadObject {
+	
+	public static void main(String ar[]){
+		Path destFilePath = Paths.get("D:/TEMP/sample.txt");
+		downloadObject("psg-sg", "psg-sg", "ntuc_attach/sample.txt", destFilePath);
+	}
+	
+  public static void downloadObject(
+		  
+		  
+      String projectId, String bucketName, String objectName, Path destFilePath) {
+    // The ID of your GCP project
+    // String projectId = "your-project-id";
+
+    // The ID of your GCS bucket
+    // String bucketName = "your-unique-bucket-name";
+
+    // The ID of your GCS object
+    // String objectName = "your-object-name";
+
+    // The path to which the file should be downloaded
+    // Path destFilePath = Paths.get("/local/path/to/file.txt");
+
+    Storage storage = StorageOptions.newBuilder().setProjectId(projectId).build().getService();
+
+    Blob blob = storage.get(BlobId.of(bucketName, objectName));
+    blob.downloadTo(destFilePath);
+
+    System.out.println(
+        "Downloaded object "
+            + objectName
+            + " from bucket name "
+            + bucketName
+            + " to "
+            + destFilePath);
+  }
+}
